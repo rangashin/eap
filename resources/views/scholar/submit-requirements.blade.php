653d38b2dff05b037d6948b0929a603183491980
rangashin <99878706+rangashin@users.noreply.github.com>
2023-01-21 23:58:18 +0800

    Update submit-requirements.blade.php

--- resources/views/scholar/submit-requirements.blade.php
+++ resources/views/scholar/submit-requirements.blade.php
@@ -38,14 +38,28 @@
                                 </thead>
                                 <tbody>
                                     <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white uppercase">Latest Registration Form/Card</th>
+                                        <th scope="row" class="flex items-center py-6 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white uppercase">
+                                            <x-input-label class=" uppercase " for="file_input_report" id="file_input_report_label" value="Latest Registration Form"/>
+                                            <svg fill="none" data-tooltip-target="tooltip-regi" data-tooltip-placement="top" class="flex-shrink-0 inline mt-2 w-5 h-5 ml-3 fill-gray-300 stroke-gray-600 mb-2 " stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"></path>
+                                            </svg>
+                                            <div id="tooltip-regi" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"><b>Example Format: </b><i> Dela Cruz, Juan-Registration Form</i>
+                                                <div class="tooltip-arrow" data-popper-arrow></div>
+                                            </div>
+                                        </th>
                                         <td class="py-4 px-6">
                                             <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" name="file_input_scholar_regi"/>
                                             <x-input-error :messages="$errors->get('file_input_scholar_regi')" class="mt-2" />
                                         </td>
                                     </tr>
                                     <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white uppercase">Latest Report Card</th>
+                                        <th scope="row" class="flex items-center py-6 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white uppercase">
+                                            <x-input-label class=" uppercase " for="file_input_report" id="file_input_report_label" value="Latest Report Card"/>
+                                            <svg fill="none" data-tooltip-target="tooltip-report" data-tooltip-placement="top" class="flex-shrink-0 inline mt-2 w-5 h-5 ml-3 fill-gray-300 stroke-gray-600 mb-2 " stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"></path>
+                                            </svg>
+                                            <div id="tooltip-report" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"><b>Example Format: </b><i> Dela Cruz, Juan-Report Card</i>
+                                                <div class="tooltip-arrow" data-popper-arrow></div>
+                                            </div>
+                                        </th>
                                         <td class="py-4 px-6">
                                             <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" name="file_input_scholar_report"/>
                                             <x-input-error :messages="$errors->get('file_input_scholar_report')" class="mt-2" />
