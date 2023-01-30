e9ea4c4723f5cb93c2511e2e8159fce21f7999f7
rangashin <99878706+rangashin@users.noreply.github.com>
2023-01-30 15:59:38 +0800

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,9 @@
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 {{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
-<img src="{{ asset('images/logo.png') }}" class="logo" alt="Laravel Logo">
+{{-- <img src="{{ asset('images/logo.png') }}" class="logo" alt="Laravel Logo"> --}}
+<img src="https://drive.google.com/uc?export=view&id=1EVJqXR_Yx_CbZ3ctz5ovEreTszsbs3xF" class="logo" alt="EAP Logo">
+
 @else
 {{ $slot }}
 @endif
